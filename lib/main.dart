90df877ee4f89330682601031e90b9274e975429
Bálint Csertán <csertanb@gmail.com>
2023-09-23 22:48:49 +0200

    Add MainAxisAlignment

--- lib/main.dart
+++ lib/main.dart
@@ -12,34 +12,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue[100],
-        appBar: AppBar(
-          title: Center(child: Text("AppBar")),
-          backgroundColor: Colors.cyan,
-          elevation: 0,
-          leading: Icon(Icons.menu),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.logout),
-            )
-          ],
-        ),
-        body: Center(
-          child: Container(
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                  color: Colors.cyan[600],
-                  borderRadius: BorderRadius.circular(25)),
-              padding: EdgeInsets.all(25),
-              child: Icon(
-                Icons.favorite,
-                color: Colors.white,
-                size: 64,
-              )),
-        ),
-      ),
+          backgroundColor: Colors.blue[100],
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              //list of widgets
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.cyan[600],
+              ),
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.cyan[400],
+              ),
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.cyan[200],
+              ),
+            ],
+          )),
     );
   }
 }
