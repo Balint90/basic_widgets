0927a860b4d22ab9540e3554f73270418bd15d38
Bálint Csertán <csertanb@gmail.com>
2023-09-23 22:14:58 +0200

    Icon, Iconstyle

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,17 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blue[100],
         body: Center(
           child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.cyan[600],
-                borderRadius: BorderRadius.circular(25)),
-            padding: EdgeInsets.all(25),
-            child: Text(
-              "Bálint Csertán",
-              style: TextStyle(
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.cyan[600],
+                  borderRadius: BorderRadius.circular(25)),
+              padding: EdgeInsets.all(25),
+              child: Icon(
+                Icons.favorite,
                 color: Colors.white,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
+                size: 64,
+              )),
         ),
       ),
     );
